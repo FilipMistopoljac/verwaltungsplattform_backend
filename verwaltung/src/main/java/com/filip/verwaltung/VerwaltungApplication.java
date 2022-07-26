de21859c4f42921eacc91a5ad7358b9bbffadf9b
f.mistopoljac <f.mistopoljac@mailbox.org>
2022-07-26 09:32:31 +0200

    1.

---
+++ verwaltung/src/main/java/com/filip/verwaltung/VerwaltungApplication.java
@@ -0,0 +1,13 @@
+package com.filip.verwaltung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerwaltungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerwaltungApplication.class, args);
+	}
+
+}
