2de98678f523658369417082cdb6ab124731c3cd
f.mistopoljac <f.mistopoljac@mailbox.org>
2022-08-02 10:24:07 +0200

    02.08.

--- project/src/main/java/com/filip/project/student/StudentService.java
+++ project/src/main/java/com/filip/project/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.filip.project.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -7,19 +8,15 @@ import java.util.List;
 @Service
 public class StudentService {
 
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents() {
-        return List.of(
-                new Student(
-                        1,
-                        "1",
-                        "SD"
-                ),
-                new Student(
-                        2,
-                        "1asdfasf",
-                        "SfsaD"
-                )
-        );
+        return studentRepository.findAll();
     }
 
 
