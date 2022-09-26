719c94954d8e77ab3af7271a3b5b9948dcdf4550
f.mistopoljac <f.mistopoljac@mailbox.org>
2022-09-26 15:10:58 +0200

    final 2

--- project/src/main/java/com/filip/project/trainer/TrainerDataLoader.java
+++ project/src/main/java/com/filip/project/trainer/TrainerDataLoader.java
@@ -14,9 +14,12 @@ public class TrainerDataLoader implements ApplicationRunner {
     public TrainerDataLoader(TrainerRepository trainerRepository) {this.trainerRepository = trainerRepository;}
 
     public void run(ApplicationArguments arg) {
-        trainerRepository.save(new Trainer("Cordula" , "Späth", "späth@com.com", "Straße", "fulltime", 2000, "FIA"));
-        trainerRepository.save(new Trainer("Mister" , "Hackerman", "späth@com.com", "Straße", "fulltime", 2000, "FIA"));
-        trainerRepository.save(new Trainer("Arnold" , "Schönberg", "späth@com.com", "Straße", "fulltime", 2000, "FIA"));
+        trainerRepository.save(new Trainer("Cordula" , "Späth", "c.späth@email.com", "Straße", "fulltime", 2000, "NT"));
+        trainerRepository.save(new Trainer("Mr." , "Hackerman", "mr.hackerman@computer.org", "Straße", "fulltime", 2000, "NT"));
+        trainerRepository.save(new Trainer("Arnold" , "Schönberg", "arnold.schönberg@gmail.com", "Straße", "fulltime", 2000, "SW"));
+        trainerRepository.save(new Trainer("Steve" , "Jobs", "steve.jobs@hotmail.com", "Straße", "fulltime", 2000, "SW"));
+        trainerRepository.save(new Trainer("Ms." , "Hackerman", "ms.hackerman@server.net", "Straße", "fulltime", 2000, "FIA"));
+        trainerRepository.save(new Trainer("Hephaistos" , "Polymetis", "hephaistos@olympos.to", "Straße", "fulltime", 2000, "FIA"));
     }
 }
 
