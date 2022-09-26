4c34177ccc643ef95249aaabdb1f8279c3786958
f.mistopoljac <f.mistopoljac@mailbox.org>
2022-09-26 04:03:39 +0200

    26

--- project/src/main/java/com/filip/project/room/RoomDataLoader.java
+++ project/src/main/java/com/filip/project/room/RoomDataLoader.java
@@ -14,8 +14,11 @@ public class RoomDataLoader implements ApplicationRunner {
     public RoomDataLoader(RoomRepository roomRepository) {this.roomRepository = roomRepository;}
 
     public void run(ApplicationArguments arg) {
-       roomRepository.save(new Room("01", "Erfinder des Betriebssystems", "William Windows"));
-       roomRepository.save(new Room("02", "Erfinder des PKW", "Elon Tesla"));
-       roomRepository.save(new Room("03", "Steampunk-Heldin", "Ada Lovelace"));
+        roomRepository.save(new Room("01", "Schutzpatronin der Handwerker und Künstler", "Athena Ergane"));
+        roomRepository.save(new Room("02", "Erfinder des Betriebssystems, einer Reihe von Computerprogrammen, welche der Anwendungssoftware Zugriff auf Systemressourcen bereitstellen", "William Windows"));
+        roomRepository.save(new Room("03", "Erfinder des sogenannten Automobils und Schutzpatron der Jungunternehmer", "Elon Tesla"));
+        roomRepository.save(new Room("04", "Steampunk-Heldin", "Ada Lovelace"));
+        roomRepository.save(new Room("05", "Programmierte nach eigenen Angaben auf Gottes Geheiß das Betriebssytem TempleOS", "Terry A. Davis"));
+        roomRepository.save(new Room("06", "Deutscher Mathematiker, trug durch seine Forschung zum Interesse Chinesischer MathematikerInnen fuer", "Karl Marx"));
     }
 }
